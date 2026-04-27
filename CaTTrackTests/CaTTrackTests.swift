1034f14e0876e87bd3f19766b31761ca1e11fe58
KamiloR7 <KamiloR7@csu.fullerton.edu>
2026-04-27 13:42:10 -0700

    Initial Commit

---
+++ CaTTrackTests/CaTTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  CaTTrackTests.swift
+//  CaTTrackTests
+//
+//  Created by Kamilo Ramirez on 4/27/26.
+//
+
+import Testing
+@testable import CaTTrack
+
+struct CaTTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
